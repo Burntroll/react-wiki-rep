eb5ac9e16b6e66de32134738366e1f0766e1550c
Francisco Jose da Silva Sousa Junior <fco.silva.dev@gmail.com>
2023-02-17 13:31:10 +0300

    Add project base

---
+++ src/components/GitHubLogo/GitHubLogo.js
@@ -0,0 +1,15 @@
+import background from '../../assets/github_logo.png';
+import React from 'react'
+
+ const GitHubLogo = () => {
+  return (
+    <img
+    src={background}
+    width={72}
+    height={72}
+    alt="Github Logo"
+  />
+  )
+}
+
+export { GitHubLogo };
---
+++ src/pages/App.js
@@ -0,0 +1,13 @@
+import { GitHubLogo } from '../components/GitHubLogo/GitHubLogo';
+import { Container } from './styles';
+
+
+function App() {
+  return (
+    <Container>
+      <GitHubLogo/>
+    </Container>
+  );
+}
+
+export default App;
